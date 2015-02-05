30524c293d2be6896e6aad21a25360cd629edd59
Creatifcookie <LaurensStuff@172-16-28-6.DYNAPOOL.NYU.EDU>
2015-02-04 21:04:14 -0500

    Chapter 2

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,42 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Lauren on 2/4/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObject:atIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // For every item in the items array ...
+        for (NSString *item in items) {
+            // Log the description of item
+            NSLog(@"%@", item);
+        }
+        
+        // Destroy the mutable array object
+        items = nil;
+        
+    }
+    return 0;
+}
+
